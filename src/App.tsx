141119bc76b2bd195108b0036c7e2a60f6794aa9
jinjongsu <jjs9836@naver.com>
2023-12-26 13:42:57 +0900

    Feat: msw 서버 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import tw from 'twin.macro';
 
 function App() {
+	useEffect(() => {
+		fetch('/api/test')
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	}, []);
 	return (
 		<div className="App">
 			<div className="text-yellow-500 p-8">Golden Ticket</div>
--- src/index.tsx
+++ src/index.tsx
@@ -11,18 +11,28 @@ const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
 );
-root.render(
-	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<RecoilRoot>
-				<App />
-			</RecoilRoot>
-			<ReactQueryDevtools />
-		</QueryClientProvider>
-	</React.StrictMode>,
-);
+async function enableMocking() {
+	if (process.env.NODE_ENV !== 'development') {
+		return;
+	}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	const { worker } = await import('./mocks/browsers');
+	return worker.start();
+}
+enableMocking().then(() => {
+	root.render(
+		<React.StrictMode>
+			<QueryClientProvider client={queryClient}>
+				<RecoilRoot>
+					<App />
+				</RecoilRoot>
+				<ReactQueryDevtools />
+			</QueryClientProvider>
+		</React.StrictMode>,
+	);
+
+	// If you want to start measuring performance in your app, pass a function
+	// to log results (for example: reportWebVitals(console.log))
+	// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+	reportWebVitals();
+});
