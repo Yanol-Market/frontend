d861a1dcf124c655e986e88bd19162806dee84f9
jinjongsu <jjs9836@naver.com>
2023-12-26 13:59:24 +0900

    Feat: App.tsx fetch 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import tw from 'twin.macro';
 
 function App() {
-	useEffect(() => {
-		fetch('/api/test')
-			.then((res) => res.json())
-			.then((data) => console.log(data));
-	}, []);
 	return (
 		<div className="App">
 			<div className="text-yellow-500 p-8">Golden Ticket</div>
