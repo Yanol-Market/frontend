fd62b990d86e25ad6cdedac53d44df075dd5f248
JungHyoJu <0092h@naver.com>
2024-01-03 12:08:20 +0900

    Feat: 공통 Header 생성

--- src/pages/main/Main.page.tsx
+++ src/pages/main/Main.page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import tw from 'twin.macro';
+import Header from '../../component/common/Header/Header';
 
 export default function Main() {
 	return (
 		<>
+			<Header title={'검색'} />
 			<div className="text-main text-headline1 font-bold py-10">메인</div>
 			<TestDiv>Golden-ticket</TestDiv>
 		</>
