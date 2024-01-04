06637dc7d8537aecd5c3ce0adc2fe6dffa8e47a3
jinjongsu <jjs9836@naver.com>
2024-01-04 18:07:46 +0900

    Feat: 불필요한 라이브러리 삭제

--- src/pages/mypage/component/BottomSheet.tsx
+++ src/pages/mypage/component/BottomSheet.tsx
@@ -17,10 +17,6 @@ const BottomSheet = ({ isOpen, onClose }: bottomSheetProps) => {
 					transition={{ type: 'spring', stiffness: 200, damping: 20 }}
 					className="border fixed absolute bottom-0 left-0 right-0 bg-white p-4 max-w-[375px] mx-auto rounded-t-3xl"
 				>
-					{/* Bottom Sheet 내용 */}
-					{/* <BottomSheetHeader /> */}
-
-					{/* 닫기 버튼 */}
 					<button
 						onClick={onClose}
 						className="mt-4 bg-gray-200 hover:bg-gray-300 border absolute right-[1.2rem]"
