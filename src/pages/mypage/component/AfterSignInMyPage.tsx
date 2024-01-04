7c5058c620ebd79b0306c2eb3c4e6255b0ecd629
jinjongsu <jjs9836@naver.com>
2024-01-04 15:37:37 +0900

    Style: 프로필 수정 페이지 수정

--- src/pages/mypage/component/AfterSignInMyPage.tsx
+++ src/pages/mypage/component/AfterSignInMyPage.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 const AfterSignInMyPage = () => {
 	return (
 		<div className="flex flex-col items-center w-full text-center">
-			<div className="mt-8">마이페이지</div>
+			<div className="mt-8 font-medium">마이페이지</div>
 			<div className="mt-11 text-start w-[90%] text-[#828282]">
 				<div className="flex flex-row gap-6">
-					<div className="border rounded-full bg-[#828282] text-[#828282] w-16 h-16" />
+					<div className="rounded-full bg-main text-[#828282] w-16 h-16" />
 					<div className="mt-2 text-[#828282] flex flex-col">
-						<p className="text-headline2 text-black">홍길동</p>
+						<p className="text-headline2 text-black font-semibold">홍길동</p>
 						<div className="flex flex-row gap-1">
-							<span>프로필 수정</span>
+							<span className="text-body">프로필 수정</span>
 							<img
-								className="cursor-pointer"
+								className="cursor-pointer w-4 h-4 mt-1"
 								src="/assets/images/rightArrow.svg"
 								alt="로그인 가기"
 							/>
@@ -21,8 +21,19 @@ const AfterSignInMyPage = () => {
 			</div>
 			<div className="border border-[#F5F5F5] bg-[#F5F5F5] w-full h-2 mt-5"></div>
 			<div className="w-[90%] mt-5 flex flex-col">
+				<div className="flex flex-row justify-between mb-1">
+					<span className="font-semibold text-lg">MY</span>
+				</div>
 				<div className="flex flex-row justify-between mb-5">
-					<span>내 계좌</span>
+					<span>판매내역</span>
+					<img
+						className="cursor-pointer"
+						src="/assets/images/rightArrowTab.svg"
+						alt="탭 이동"
+					/>
+				</div>
+				<div className="flex flex-row justify-between mb-5">
+					<span>구매내역</span>
 					<img
 						className="cursor-pointer"
 						src="/assets/images/rightArrowTab.svg"
@@ -38,12 +49,34 @@ const AfterSignInMyPage = () => {
 					/>
 				</div>
 				<div className="flex flex-row justify-between mb-5">
-					<span>설정</span>
+					<span>관심지역</span>
 					<img
 						className="cursor-pointer"
 						src="/assets/images/rightArrowTab.svg"
 						alt="탭 이동"
 					/>
+				</div>
+				<div className="flex flex-row justify-between mt-1 mb-1">
+					<span className="font-semibold text-lg">관리</span>
+				</div>
+				<div className="flex flex-row justify-between mb-5">
+					<span>내 계좌</span>
+					<img
+						className="cursor-pointer"
+						src="/assets/images/rightArrowTab.svg"
+						alt="탭 이동"
+					/>
+				</div>
+				<div className="flex flex-row justify-between mb-5">
+					<span>계정관리</span>
+					<img
+						className="cursor-pointer"
+						src="/assets/images/rightArrowTab.svg"
+						alt="탭 이동"
+					/>
+				</div>
+				<div className="flex flex-row justify-between mt-1 mb-1">
+					<span className="font-semibold text-lg">서비스</span>
 				</div>
 				<div className="flex flex-row justify-between mb-5">
 					<span>공지사항</span>
--- src/pages/mypage/component/ProfileEdit.tsx
+++ src/pages/mypage/component/ProfileEdit.tsx
@@ -9,58 +9,40 @@ const ProfileEdit = () => {
 					src="/assets/images/leftArrow.svg"
 					alt="왼쪽으로 가기"
 				/>
-				<p className="mx-auto text-botton font-bold">프로필 수정</p>
+				<p className="mx-auto text-lg font-medium">프로필 수정</p>
 			</div>
 			<div className="border border-borderGray mt-11 rounded-full w-24 h-24" />
 			<div className="w-[90%] mt-14">
 				<form>
-					<div>
+					<div className="relative">
 						<div className="flex flex-row justify-between font-bold">
-							<p className="text-botton">닉네임</p>
+							<p className="text-lg">닉네임</p>
 							<button
-								className="border border-borderGray w-[52px] bg-borderGray text-sm text-[#828282] rounded-md"
+								className="border border-borderGray absolute cursor-pointer right-2 top-[2.5rem] w-14 h-6 bg-borderGray text-sm text-[#828282] rounded-md"
 								type="button"
 							>
 								중복 확인
 							</button>
 						</div>
 						<input
-							className="w-full h-11 rounded-xl text-botton mt-2 bg-[#FAFAFA] pl-4 focus:outline-none"
+							className="w-full h-11 rounded-xl text-lg mt-2 bg-[#FAFAFA] pl-4 focus:outline-none"
 							type="text"
 							placeholder="원래 이름"
 						/>
 					</div>
 					<div className="mt-7">
 						<div className="flex flex-row justify-between font-bold">
-							<p className="text-botton">휴대폰</p>
+							<p className="text-lg">휴대폰 번호</p>
 						</div>
-						<input
-							className="w-full h-11 rounded-xl text-botton mt-2 bg-[#FAFAFA] pl-4 focus:outline-none"
-							type="text"
-							placeholder="원래 이름"
-						/>
-					</div>
-					<div className="mt-7">
-						<div className="flex flex-row justify-between font-bold">
-							<p className="text-botton">관심지역</p>
+						<div className="w-full h-11 rounded-xl text-botton mt-2 bg-[#FAFAFA] pl-4 focus:outline-none">
+							<p className="pt-2 text-start text-gray">010-1234-5678</p>
 						</div>
-
-						<select
-							className="w-full h-11 rounded-xl text-botton mt-2 bg-[#FAFAFA] pl-4 focus:outline-none text-gray"
-							name="관심 지역"
-						>
-							<option value="" selected hidden disabled>
-								관심 지역을 선택해주세요.
-							</option>
-							<option>하이</option>
-							<option>바이</option>
-						</select>
 					</div>
 					<button
 						type="submit"
 						className="mt-44 flex items-center w-full h-11 rounded-xl text-center text-m bg-main text-white"
 					>
-						<span className="mx-auto">가입하기</span>
+						<span className="mx-auto">변경사항 저장하기</span>
 					</button>
 				</form>
 			</div>
