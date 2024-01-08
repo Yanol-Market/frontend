5f708ecdf944b544d8fb6eac94921e0ce85a5269
moonyah <dltk456@naver.com>
2024-01-08 21:33:32 +0900

    Feat: 약관 및 정책 동의 페이지

--- src/pages/addProduct/component/TermsAndPolicyAgreementStep.tsx
+++ src/pages/addProduct/component/TermsAndPolicyAgreementStep.tsx
@@ -1,5 +1,4 @@
-// TermsAndPolicyAgreementStep.js
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
 	onPrevStep: () => void;
@@ -7,14 +6,71 @@ interface Props {
 }
 
 const TermsAndPolicyAgreementStep = ({ onPrevStep, onComplete }: Props) => {
-	// 필요한 로직 및 상태 관리
+	const [agreed, setAgreed] = useState(false);
 
 	return (
 		<div>
-			<h2>약관 및 정책 동의</h2>
-			{/* 약관 동의 관련 컴포넌트 및 로직 */}
-			<button onClick={onPrevStep}>이전 단계</button>
-			<button onClick={onComplete}>등록 완료</button>
+			<div>
+				<h2 className="text-body ml-5 mb-4 fixed top-[6.5rem]">
+					약관 및 정책 동의
+				</h2>
+				<div className="fixed top-[9rem] w-[375px] h-[0.4375rem] bg-[#FAFAFA]" />
+				<div className="flex flex-col h-screen text-black">
+					<h3 className="text-body font-semibold mt-[11rem] ml-5 my-5">
+						약관 및 정책
+					</h3>
+					{/* 약관 및 정책 내용 */}
+					<div className="bg-lightGray p-4 rounded-xl max-w-[335px] mx-auto mb-4 text-sm">
+						<ul>
+							<li className="text-alarmRed mb-4">
+								• 판매하고자 하는 상품은 양도 가능한 상품임을 확인하였으며, 양도
+								불가 상품 판매 시 페널티가 발생 수 있습니다.
+							</li>
+							<li className="mb-4">
+								• 골든티켓에 등록하신 상품을 다른 곳에서 이중판매하실 경우
+								발생한 불이익에 대해서 자사는 책임지지 않으며 이미 판매된
+								예약권에 대한 소유권은 골든티켓 고객에게 우선적으로 있음을
+								동의합니다.
+							</li>
+							<li>
+								• 판매등록 후 상품 검수, 양도 시 발생하는 확인절차와 양도절차에
+								대한 업무는 전적으로 골든티켓에 위임함에 동의합니다.
+							</li>
+						</ul>
+					</div>
+
+					{/* 동의 체크박스 */}
+					<div className="flex flex-col mx-auto">
+						<div className="flex mb-3">
+							<input
+								type="checkbox"
+								className="mt-[0.15rem] appearance-none bg-[url('pages/addProduct/component/unchecked.svg')] w-4 h-4 mr-2 checked:bg-[url('pages/addProduct/component/checked.svg')]"
+								checked={agreed}
+								onChange={() => setAgreed(!agreed)}
+							/>
+							<label className="font-medium text-lg">
+								위 내용을 숙지하였고 동의합니다.(필수)
+							</label>
+						</div>
+						<p className="text-sm mx-auto">
+							비동의 시 골든티켓에서의 판매가 불가합니다.
+						</p>
+					</div>
+
+					{/* 다음 버튼 */}
+					<div className="fixed bottom-7 left-0 right-0 bg-gray-200 flex justify-center">
+						<button
+							type="button"
+							className={`mx-auto bg-borderGray w-[20.9375rem] h-[3.125rem] rounded-xl text-lg ${
+								agreed ? 'text-white bg-main cursor-pointer' : 'text-[#828282]'
+							}`}
+							onClick={agreed ? onComplete : undefined}
+						>
+							다음
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
