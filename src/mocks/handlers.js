ce63779ef0ab6caca3e3a837dd4ff7e4bc4898f5
jinjongsu <jjs9836@naver.com>
2024-01-10 14:18:44 +0900

    Feat: 로그인/회원가입 msw 셋팅

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,4 +1,5 @@
 import { http, HttpResponse } from 'msw';
+import { v4 as uuidv4 } from 'uuid';
 
 export const handlers = [
 	http.get('/api/test', () => {
@@ -21,25 +22,31 @@ export const handlers = [
 	}),
 	http.post('/api/signin', async ({ request }) => {
 		const userInfo = await request.json();
-
+		const randomAccessToken = uuidv4();
+		const randomRefreshToken = uuidv4();
 		return HttpResponse.json([
 			{
 				data: {
-					email: userInfo.email,
-					password: userInfo.password,
-					accessToken: 'myAccessToken',
-					refreshToken: 'myRefreshToken',
+					grantType: 'Bearer',
+					accessToken: randomAccessToken,
+					refreshToken: randomRefreshToken,
+					expiresIn: 3600,
 				},
-				status: 201,
+				status: 'SUCCESS',
 			},
 		]);
 	}),
 	http.post('/api/signup', async ({ request }) => {
 		const userInfo = await request.json();
+		const randomId = uuidv4(); // 실제 데이터는 number로 받아오지만 임시로 string 처리
 		if (userInfo) {
 			return HttpResponse.json([
 				{
-					status: 201,
+					data: {
+						id: randomId,
+						name: userInfo.name,
+						email: userInfo.email,
+					},
 				},
 			]);
 		}
@@ -51,69 +58,68 @@ export const handlers = [
 
 const products = [
 	{
-		productId  : 1,
-		name  : '속초 씨 크루즈 호텔',
-		option  : '스탠다드 하프 오션뷰 더블룸',
-		checkIn  :"2022-12-31 23:59:59",
-		checkOut  : "2022-12-31 23:59:59",
-		dDay : 2,
-		marketPrice : 240000,
-		marketPriceRatio : 34,
-		purchasePrice  : 213000,
-		purchasePriceRatio  : 25,
-		price  : 159000,
+		productId: 1,
+		name: '속초 씨 크루즈 호텔',
+		option: '스탠다드 하프 오션뷰 더블룸',
+		checkIn: '2022-12-31 23:59:59',
+		checkOut: '2022-12-31 23:59:59',
+		dDay: 2,
+		marketPrice: 240000,
+		marketPriceRatio: 34,
+		purchasePrice: 213000,
+		purchasePriceRatio: 25,
+		price: 159000,
 	},
 	{
-		productId  : 2,
-		name  : '블루 하와이 리조트',
-		option  : '트윈룸 오션뷰',
-		checkIn  :"2022-12-31 23:59:59",
-		checkOut  : "2022-12-31 23:59:59",
-		dDay : 1,
-		marketPrice : 240000,
-		marketPriceRatio : 17,
-		purchasePrice  : 213000,
-		purchasePriceRatio  : 6,
-		price  : 200000,
+		productId: 2,
+		name: '블루 하와이 리조트',
+		option: '트윈룸 오션뷰',
+		checkIn: '2022-12-31 23:59:59',
+		checkOut: '2022-12-31 23:59:59',
+		dDay: 1,
+		marketPrice: 240000,
+		marketPriceRatio: 17,
+		purchasePrice: 213000,
+		purchasePriceRatio: 6,
+		price: 200000,
 	},
 	{
-		productId  : 2,
-		name  : '블루 하와이 리조트',
-		option  : '트윈룸 오션뷰',
-		checkIn  :"2022-12-31 23:59:59",
-		checkOut  : "2022-12-31 23:59:59",
-		dDay : 1,
-		marketPrice : 240000,
-		marketPriceRatio : 17,
-		purchasePrice  : 213000,
-		purchasePriceRatio  : 6,
-		price  : 200000,
+		productId: 2,
+		name: '블루 하와이 리조트',
+		option: '트윈룸 오션뷰',
+		checkIn: '2022-12-31 23:59:59',
+		checkOut: '2022-12-31 23:59:59',
+		dDay: 1,
+		marketPrice: 240000,
+		marketPriceRatio: 17,
+		purchasePrice: 213000,
+		purchasePriceRatio: 6,
+		price: 200000,
 	},
 	{
-		productId  : 2,
-		name  : '블루 하와이 리조트',
-		option  : '트윈룸 오션뷰',
-		checkIn  :"2022-12-31 23:59:59",
-		checkOut  : "2022-12-31 23:59:59",
-		dDay : 1,
-		marketPrice : 240000,
-		marketPriceRatio : 17,
-		purchasePrice  : 213000,
-		purchasePriceRatio  : 6,
-		price  : 200000,
+		productId: 2,
+		name: '블루 하와이 리조트',
+		option: '트윈룸 오션뷰',
+		checkIn: '2022-12-31 23:59:59',
+		checkOut: '2022-12-31 23:59:59',
+		dDay: 1,
+		marketPrice: 240000,
+		marketPriceRatio: 17,
+		purchasePrice: 213000,
+		purchasePriceRatio: 6,
+		price: 200000,
 	},
 	{
-		productId  : 2,
-		name  : '블루 하와이 리조트',
-		option  : '트윈룸 오션뷰',
-		checkIn  :"2022-12-31 23:59:59",
-		checkOut  : "2022-12-31 23:59:59",
-		dDay : 1,
-		marketPrice : 240000,
-		marketPriceRatio : 17,
-		purchasePrice  : 213000,
-		purchasePriceRatio  : 6,
-		price  : 200000,
+		productId: 2,
+		name: '블루 하와이 리조트',
+		option: '트윈룸 오션뷰',
+		checkIn: '2022-12-31 23:59:59',
+		checkOut: '2022-12-31 23:59:59',
+		dDay: 1,
+		marketPrice: 240000,
+		marketPriceRatio: 17,
+		purchasePrice: 213000,
+		purchasePriceRatio: 6,
+		price: 200000,
 	},
-	
-]
+];
