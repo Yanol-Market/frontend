298cc7a97a063214b20428c150c961fbbb50db27
JungHyoJu <0092h@naver.com>
2024-01-11 15:45:14 +0900

    Feat : 나의 구매현황 생성

--- src/pages/mypage/component/salesHistory/Buyer.tsx
+++ src/pages/mypage/component/salesHistory/Buyer.tsx
@@ -2,27 +2,35 @@ import React, { useState } from 'react';
 import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
 import ExpandLessRoundedIcon from '@mui/icons-material/ExpandLessRounded';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { useLocation } from 'react-router-dom';
 
 const Buyer = () => {
 	const [click, setClick] = useState(true);
-
+	const location = useLocation();
+	const currentPath = location.pathname;
 	return (
 		<div className="pt-5">
 			<div className="flex mb-4">
-				<div>구매 희망자 </div>{' '}
-				<div>
-					{click ? (
-						<ExpandMoreOutlinedIcon
-							onClick={() => setClick(!click)}
-							className="cursor-pointer"
-						/>
-					) : (
-						<ExpandLessRoundedIcon
-							onClick={() => setClick(!click)}
-							className="cursor-pointer"
-						/>
-					)}
-				</div>
+				{currentPath === '/purchase' ? (
+					<div className="text-body font-semibold"> 나의 구매현황 </div>
+				) : (
+					<>
+						<div className="text-body font-semibold">구매 희망자 </div>
+						<div>
+							{click ? (
+								<ExpandMoreOutlinedIcon
+									onClick={() => setClick(!click)}
+									className="cursor-pointer"
+								/>
+							) : (
+								<ExpandLessRoundedIcon
+									onClick={() => setClick(!click)}
+									className="cursor-pointer"
+								/>
+							)}
+						</div>
+					</>
+				)}
 			</div>
 			{click ? null : (
 				<div className="border-borderGray border px-5 py-4 mt-2 flex items-center  justify-between rounded-lg">
