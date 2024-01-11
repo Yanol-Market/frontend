bc22d7005bdb002b9e52e629e7a14ee438034335
JungHyoJu <0092h@naver.com>
2024-01-11 15:46:07 +0900

    Feat : 구매중 UI 생성

--- src/pages/mypage/component/purchaseHistory/Buying.tsx
+++ src/pages/mypage/component/purchaseHistory/Buying.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import SalesProduct from '../salesHistory/SalesProduct';
+import StatusBar from '../salesHistory/StatusBar';
+import Buyer from '../salesHistory/Buyer';
 
 const Buying = () => {
-	return <div>Buying</div>;
+	return (
+		<div className="p-5">
+			<div className="pb-5 flex justify-between items-center">
+				<p className="text-sm pt-[8px]">골든티켓 등록번호 4567894512</p>
+			</div>
+			<SalesProduct />
+			<StatusBar />
+			<Buyer />
+		</div>
+	);
 };
 
 export default Buying;
