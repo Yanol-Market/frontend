7257049c6403c88f878688cd0c8e7a571d92ebcd
moonyah <dltk456@naver.com>
2024-01-17 17:03:12 +0900

    Feat: 상품 정보 입력  단계 퍼블리싱 개선

--- src/pages/addProduct/component/infoInputStep/AddProductInfoInputStep.tsx
+++ src/pages/addProduct/component/infoInputStep/AddProductInfoInputStep.tsx
@@ -9,49 +9,66 @@ interface Props {
 }
 
 const AddProductInfoInputStep = ({ onNextStep }: Props) => {
-	const [desiredPrice, setDesiredPrice] = useState<number>(0);
+	const [goldenPrice, setGoldenPrice] = useState<number>(-1);
+	const originPrice = 200000;
+
+	const handleNextStep = () => {
+		if (goldenPrice >= 0 && goldenPrice < originPrice) {
+			onNextStep();
+		} else {
+			let alertMessage = '';
+			if (goldenPrice < 0) {
+				alertMessage = '희망 판매가를 입력해주세요!';
+			} else if (goldenPrice >= originPrice) {
+				alertMessage = '희망 판매가는 기존 구매가보다 낮아야 합니다!';
+			}
+			alert(alertMessage);
+		}
+	};
 
 	function setSellerInfo(info: string): void {
-		throw new Error('동작하지 않음');
+		throw new Error('Function not implemented.');
 	}
 
 	return (
-		<div>
+		<>
 			<div className="fixed top-[6rem] w-[375px] bg-white h-[3rem]" />
 			<h2 className="text-body ml-5 mb-4 fixed top-[6.5rem]">판매 정보 입력</h2>
 			<div className="fixed top-[9rem] w-[375px] h-[0.4375rem] bg-lightGray" />
 
-			{/* 없애면 ui가 이상하게 변함 코드 수정 필요 */}
-			<p>Checking if this text is visible</p>
-
 			{/* 골든특가 결정 */}
-			<div className="mt-[9.5rem] mx-5 text-black">
+			<div className="pt-[11rem] px-5">
 				<GoldenPriceDecision
-					currentSellingPrice={100000}
-					onPriceChange={(newPrice) => setDesiredPrice(newPrice)}
-					initialPurchasePrice={200000}
+					yanoljaPrice="100,000"
+					setGoldenPrice={setGoldenPrice}
+					originPrice="200,000"
 				/>
 			</div>
+
 			<div className="mt-[2rem] w-[375px] h-[0.4375rem] bg-lightGray" />
 			{/* 판매자 한마디 */}
 			<div className="mx-5 text-black">
-				<SellerInfoInput onSellerInfoChange={(info) => setSellerInfo(info)} />
+				<SellerInfoInput onSellerInfoChange={setSellerInfo} />
 			</div>
 			{/* 수수료 안내 */}
 			<div className="mx-5 text-black">
-				<CommissionInfo desiredPrice={desiredPrice} />
+				<CommissionInfo goldenPrice={goldenPrice} />
 			</div>
 			{/* 다음 버튼 */}
 			<div className="bg-gray-200 flex justify-center">
 				<button
 					type="button"
-					className="mx-auto bg-borderGray w-[20.9375rem] h-[3.125rem] rounded-xl text-descGray cursor-pointer text-lg mt-[3.5625rem] mb-[2.1875rem]"
-					onClick={onNextStep}
+					className={`mx-auto w-[20.9375rem] h-[3.125rem] rounded-xl text-lg mt-[3.5625rem] mb-[2.1875rem] ${
+						goldenPrice >= 0 && goldenPrice < originPrice
+							? 'cursor-pointer bg-main text-white'
+							: 'cursor-not-allowed bg-borderGray text-descGray'
+					}`}
+					onClick={handleNextStep}
 				>
 					다음
 				</button>
 			</div>
-		</div>
+		</>
 	);
 };
 
--- src/pages/addProduct/component/infoInputStep/CommissionInfo.tsx
+++ src/pages/addProduct/component/infoInputStep/CommissionInfo.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
 interface CommissionInfoProps {
-	desiredPrice: number;
+	goldenPrice: number;
 }
 
-const CommissionInfo: React.FC<CommissionInfoProps> = ({ desiredPrice }) => {
+const formatNumber = (number: number) => {
+	const formattedNumber = number.toFixed(0);
+	return formattedNumber.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+const CommissionInfo = ({ goldenPrice }: CommissionInfoProps) => {
 	// 골든티켓 수수료율
 	const commissionRate = 5;
 
 	// 골든티켓 수수료 계산
-	const commissionAmount = (desiredPrice * commissionRate) / 100;
+	const commissionAmount = (goldenPrice * commissionRate) / 100;
 
 	// 예상 최종 정산액 계산
-	const finalSettlementAmount = desiredPrice - commissionAmount;
+	const finalSettlementAmount = goldenPrice - commissionAmount;
 
 	return (
 		<div className="mt-5">
@@ -20,17 +25,19 @@ const CommissionInfo: React.FC<CommissionInfoProps> = ({ desiredPrice }) => {
 			<div className="mt-4 rounded-md bg-bgMain p-4 text-lg">
 				<p className="flex justify-between">
 					<span>희망 판매가</span>
-					<span className="font-semibold">{desiredPrice}원</span>
+					<span className="font-semibold">{formatNumber(goldenPrice)}원</span>
 				</p>
 				<p className="flex justify-between mt-2">
 					<span>골든티켓 수수료</span>
 					<span className="font-semibold">
-						-{commissionAmount}원 ({commissionRate}%)
+						-{formatNumber(commissionAmount)}원 ({commissionRate}%)
 					</span>
 				</p>
 				<p className="flex justify-between font-semibold text-dateBlue mt-2">
 					<span>예상 최종 정산액</span>
-					<span className="font-semibold">{finalSettlementAmount}원</span>
+					<span className="font-semibold">
+						{formatNumber(finalSettlementAmount)}원
+					</span>
 				</p>
 			</div>
 		</div>
