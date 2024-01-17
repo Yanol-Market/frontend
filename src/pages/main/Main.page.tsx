54c771cdd22be3827d69e1b277992887e38cef72
jinjongsu <jjs9836@naver.com>
2024-01-18 01:02:32 +0900

    Feat: 테스트 코드 제거

--- src/pages/main/Main.page.tsx
+++ src/pages/main/Main.page.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import { HomeHeader } from './component/HomeHeader/HomeHeader';
 import { HomeContent } from './component/HomeContent';
-import { getCookie } from '../../apis/cookie';
-import { isTokenExpired } from '../../utils/tokenExpireCheck';
 
 export default function Main() {
-	const accessToken = getCookie('accessToken');
-	const isExpired = isTokenExpired(accessToken);
-	if (isExpired) {
-		console.log('토큰이 만료되었습니다');
-	} else {
-		console.log('토큰이 유효합니다');
-	}
 	return (
 		<>
 			<HomeHeader />
