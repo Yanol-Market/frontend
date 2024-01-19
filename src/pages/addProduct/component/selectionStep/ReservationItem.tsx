120e8e26a568720bc6b92ee6e6f775c57e9c6e03
moonyah <dltk456@naver.com>
2024-01-19 17:12:19 +0900

    Feat: 예약 목록 조회 api 연결

--- src/pages/addProduct/component/selectionStep/ReservationItem.tsx
+++ src/pages/addProduct/component/selectionStep/ReservationItem.tsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 
 export interface Reservation {
-	reservationNumber: number;
-	reservationDate: string;
-	hotelName: string;
-	numberOfPeople: number; // 인원 수
-	maxNumberOfPeople: number; // 최대 인원 수
-	roomInfo: string;
-	totalAmount: string;
+	reservationId: number;
+	reservationStatus: string; // 상품 등록 여부
+	accommodationName: string;
+	reservationType: string;
+	roomName: string;
+	standardNumber: number; // 인원 수
+	maximumNumber: number; // 최대 인원 수
 	checkInDate: string;
 	checkOutDate: string;
 	checkInTime: string;
 	checkOutTime: string;
-	accommodationType: string;
-	isRegistered: boolean; // 상품 등록 여부
+	nights: number;
+	reservationDate: string;
+
+	originPrice: number;
+	yanoljaPrice: number;
 }
 
 interface ReservationItemProps {
@@ -22,12 +25,27 @@ interface ReservationItemProps {
 	onClick: () => void;
 }
 
+const formatDate = (dateString: string) => {
+	const dateObject = new Date(dateString);
+	const year = dateObject.getFullYear();
+	const month = String(dateObject.getMonth() + 1).padStart(2, '0');
+	const day = String(dateObject.getDate()).padStart(2, '0');
+	return `${year}.${month}.${day}`;
+};
+
+const formatNumberWithCommas = (number: number) => {
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
 const ReservationItem = ({
 	reservation,
 	isSelected,
 	onClick,
 }: ReservationItemProps) => {
-	const isRegistered = reservation.isRegistered;
+	const isRegistered = reservation.reservationStatus === 'REGISTERED';
+
+	const formattedReservationType =
+		reservation.reservationType === 'DAY_USE' ? '대실' : '숙박';
 
 	const handleClick = () => {
 		if (isRegistered) {
@@ -57,8 +75,8 @@ const ReservationItem = ({
 							: 'text-descGray'
 				}`}
 			>
-				<p className="mr-2 text-sm">예약번호 {reservation.reservationNumber}</p>
-				<p className="text-sm">{reservation.reservationDate}</p>
+				<p className="mr-2 text-sm">예약번호 {reservation.reservationId}</p>
+				<p className="text-sm">{formatDate(reservation.reservationDate)}</p>
 			</div>
 			<div
 				className={`border-b w-full mt-2 mb-[0.8125rem] ${
@@ -68,7 +86,7 @@ const ReservationItem = ({
 			<div className="flex flex-col">
 				<div className="flex items-center justify-between">
 					<p className={`text-lg font-semibold mb-[0.3125rem]}`}>
-						{reservation.hotelName}
+						{reservation.accommodationName}
 					</p>
 					<p
 						className={`text-sm border rounded-full px-[0.4rem] py-0.5  ${
@@ -77,27 +95,30 @@ const ReservationItem = ({
 								: 'border-borderGray bg-lightGray'
 						}`}
 					>
-						{reservation.accommodationType}
+						{formattedReservationType}
 					</p>
 				</div>
 				<p className="text-sm flex items-center">
-					<span>{reservation.roomInfo}</span>
+					<span>{reservation.roomName}</span>
 					<div
 						className={`h-2 w-[0.01rem] mx-[0.4rem]  ${
 							isSelected ? 'bg-homeMain' : 'bg-borderGray'
 						}`}
 					/>
-					<span>{`${reservation.numberOfPeople}인`}</span>
+					<span>{`${reservation.standardNumber}인`}</span>
 					<div
 						className={`h-2 w-[0.01rem] mx-[0.4rem]  ${
 							isSelected ? 'bg-homeMain' : 'bg-borderGray'
 						}`}
-					/>{' '}
-					<span>{`최대 ${reservation.maxNumberOfPeople}인`}</span>
+					/>
+					<span>{`최대 ${reservation.maximumNumber}인`}</span>
 				</p>
 			</div>
 			<div className="flex items-end flex-col text-m">
-				<p className="mb-[0.625rem]">{reservation.totalAmount}</p>
+				<p className="mb-[0.625rem]">
+					{reservation.nights !== 0 && `${reservation.nights}박 `}
+					{formatNumberWithCommas(reservation.originPrice)}원
+				</p>
 				<div
 					className={`items-center flex w-full h-[3.875rem] rounded-md ${
 						isRegistered
@@ -111,7 +132,9 @@ const ReservationItem = ({
 						{/* 체크인 정보 */}
 						<div className="text-center">
 							<p className="font-semibold mb-[0.35rem]">체크인</p>
-							<p className="whitespace-pre">{`${reservation.checkInDate}  ${reservation.checkInTime}`}</p>
+							<p className="whitespace-pre">{`${formatDate(
+								reservation.checkInDate,
+							)}   ${reservation.checkInTime.slice(0, 5)}`}</p>
 						</div>
 					</div>
 					<div
@@ -123,7 +146,9 @@ const ReservationItem = ({
 						{/* 체크아웃 정보 */}
 						<div className="text-center">
 							<p className="font-semibold mb-[0.35rem]">체크아웃</p>
-							<p className="whitespace-pre">{`${reservation.checkOutDate}  ${reservation.checkOutTime}`}</p>
+							<p className="whitespace-pre">{`${formatDate(
+								reservation.checkOutDate,
+							)}   ${reservation.checkOutTime.slice(0, 5)}`}</p>
 						</div>
 					</div>
 				</div>
