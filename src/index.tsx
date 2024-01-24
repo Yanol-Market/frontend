6f7a79d72d91d4cd3c639c0a2495d8f92fd635ff
KSJ <https://github.com/KSJT>
2024-01-24 14:48:40 +0900

    Fix: strict mode on

--- src/index.tsx
+++ src/index.tsx
@@ -23,18 +23,19 @@ async function enableMocking() {
 }
 enableMocking().then(() => {
 	root.render(
-		<QueryClientProvider client={queryClient}>
-			<RecoilRoot>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
-			</RecoilRoot>
-			<ReactQueryDevtools />
-		</QueryClientProvider>,
+		<React.StrictMode>
+			<CookiesProvider>
+				<QueryClientProvider client={queryClient}>
+					<RecoilRoot>
+						<BrowserRouter>
+							<App />
+						</BrowserRouter>
+					</RecoilRoot>
+					<ReactQueryDevtools />
+				</QueryClientProvider>
+			</CookiesProvider>
+		</React.StrictMode>,
 	);
 
-	// If you want to start measuring performance in your app, pass a function
-	// to log results (for example: reportWebVitals(console.log))
-	// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 	reportWebVitals();
 });
