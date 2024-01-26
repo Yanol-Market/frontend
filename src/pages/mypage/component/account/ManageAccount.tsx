8e25e4bc57f23541cdb0a4b28b81a939bce90b84
jinjongsu <jjs9836@naver.com>
2024-01-26 11:01:21 +0900

    Fix: 로그아웃 바텀시트 높이 조절

--- src/pages/mypage/component/account/ManageAccount.tsx
+++ src/pages/mypage/component/account/ManageAccount.tsx
@@ -54,7 +54,7 @@ const ManageAccount = () => {
 			<BottomSheet
 				isOpen={isBottomSheetSignOutOpen}
 				onClose={closeBottomSheetSignOut}
-				viewHeight="calc(100vh * 0.33)"
+				viewHeight="calc(100vh * 0.31)"
 			>
 				<ContentTwoBtnPage
 					title="로그아웃 하시겠습니까?"
