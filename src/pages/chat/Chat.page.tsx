d2b8aa4d6f9d7ef90a00e214e4ad4d3dc1e26be8
KSJ <https://github.com/KSJT>
2024-01-28 21:17:45 +0900

    Fix: 로그 제거

--- src/pages/chat/Chat.page.tsx
+++ src/pages/chat/Chat.page.tsx
@@ -83,7 +83,6 @@ const ChatPage = () => {
 	useEffect(() => {
 		if (chatRoomData) {
 			const { chatRoomInfoResponse, chatResponseList } = chatRoomData;
-			console.log(chatRoomData);
 			setProductData(chatRoomInfoResponse);
 			setChatList(chatResponseList);
 
